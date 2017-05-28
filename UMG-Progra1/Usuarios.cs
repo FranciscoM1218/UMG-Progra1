92dca76eba9b9f2b341e6d4596c71e5c8100236b
FranciscoM1218 <fran_cisco7@hotmail.com>
2017-05-28 09:26:24 -0600

    Crear vista Usuarios

---
+++ UMG-Progra1/Usuarios.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UMG_Progra1
+{
+    public partial class Usuarios : Form
+    {
+        public Usuarios()
+        {
+            InitializeComponent();
+        }
+
+        private void Usuarios_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
