2139319188814a425ad6c51411fbc11d9febc8e1
fernandodavila <luismontenegrod@hotmail.com>
2017-05-28 14:52:37 -0600

    agregar tabla a la vista usuarios

--- UMG-Progra1/Usuarios.Designer.cs
+++ UMG-Progra1/Usuarios.Designer.cs
@@ -28,20 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.id_user = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.DPI = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.name = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.email = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.password = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.age = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.admin = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.id_user,
+            this.DPI,
+            this.name,
+            this.email,
+            this.password,
+            this.age,
+            this.admin});
+            this.dataGridView1.Location = new System.Drawing.Point(12, 12);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(710, 150);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // id_user
+            // 
+            this.id_user.HeaderText = "ID";
+            this.id_user.Name = "id_user";
+            this.id_user.ReadOnly = true;
+            // 
+            // DPI
+            // 
+            this.DPI.HeaderText = "DPI";
+            this.DPI.Name = "DPI";
+            // 
+            // name
+            // 
+            this.name.HeaderText = "Nombre";
+            this.name.Name = "name";
+            // 
+            // email
+            // 
+            this.email.HeaderText = "Correo electronico";
+            this.email.Name = "email";
+            // 
+            // password
+            // 
+            this.password.HeaderText = "Contraseña ";
+            this.password.Name = "password";
+            // 
+            // age
+            // 
+            this.age.HeaderText = "Edad";
+            this.age.Name = "age";
+            // 
+            // admin
+            // 
+            this.admin.HeaderText = "Admin";
+            this.admin.Name = "admin";
             // 
             // Usuarios
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.ClientSize = new System.Drawing.Size(752, 261);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "Usuarios";
-            this.Text = "Usuarios";
+            this.Text = " ";
             this.Load += new System.EventHandler(this.Usuarios_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn id_user;
+        private System.Windows.Forms.DataGridViewTextBoxColumn DPI;
+        private System.Windows.Forms.DataGridViewTextBoxColumn name;
+        private System.Windows.Forms.DataGridViewTextBoxColumn email;
+        private System.Windows.Forms.DataGridViewTextBoxColumn password;
+        private System.Windows.Forms.DataGridViewTextBoxColumn age;
+        private System.Windows.Forms.DataGridViewTextBoxColumn admin;
     }
 }
